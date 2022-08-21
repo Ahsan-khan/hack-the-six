ad7e779f48e3ff1b72a04fc129bc3f18030f88c8
srivatsan <srivatsan25297@gmail.com>
2022-08-21 09:32:14 -0400

    added code for processing image through vision api

--- upload.js
+++ upload.js
@@ -1,5 +1,7 @@
 // Create namespace object:
 const app = {};
+const vision = require("@google-cloud/vision");
+  
 
 app.displayImage = () => {
     $('#photo').change(function(){
@@ -9,6 +11,7 @@ app.displayImage = () => {
           let reader = new FileReader();
           reader.onload = function(event){
             console.log(event.target.result);
+            response = quickstart(event.target.result)
             $('#imgPreview').attr('src', event.target.result);
           }
           reader.readAsDataURL(file);
@@ -19,6 +22,25 @@ app.displayImage = () => {
 // Create initialization code:
 app.init = () => {
     app.displayImage();
+}
+
+// vision api call
+async function quickstart(require, image_data) {
+    const objects = []
+  // Performs label detection on the image file
+    const [result] = await client.localizedObjectAnnotations(image_data);
+     objects = result.objects.forEach(object => {
+      console.log(`Name: ${object.name}`);
+      console.log(`Confidence: ${object.score}`);
+      const vertices = object.boundingPoly.normalizedVertices;
+      vertices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
+  // return objects
+  });
+  console.log(objects)
+
+
+  // Creates a client
+  
 }
 
 
